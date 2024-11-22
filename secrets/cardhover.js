144c70e2f049300fee075965cb921707e753f716
MilkyBowls <82846577+MilkyBowls@users.noreply.github.com>
2024-11-22 16:31:33 -0500

    cool cards bro

--- secrets/cardhover.js
+++ secrets/cardhover.js
@@ -1,12 +1,14 @@
-const card = document.querySelector('.card')
+const cards = document.querySelectorAll('.card');
 
-card.addEventListener('mousemove', event => {
-  const y = event.offsetY - 180,
-        xRotation = y / -360,
-        x = event.offsetX - 180,
-        yRotation = x / 360,
-        brightness = (Math.abs(-360 + x + y) / 360) + .5;
-  card.style.setProperty('--x-rotation', xRotation + 'rad')
-  card.style.setProperty('--y-rotation', yRotation + 'rad')
-  card.style.setProperty('--brightness', brightness)
-})
+cards.forEach(card => {
+  card.addEventListener('mousemove', event => {
+    const y = event.offsetY - 180,
+          xRotation = y / -360,
+          x = event.offsetX - 180,
+          yRotation = x / 360,
+          brightness = (Math.abs(-360 + x + y) / 360) + 0.5;
+    card.style.setProperty('--x-rotation', `${xRotation}rad`);
+    card.style.setProperty('--y-rotation', `${yRotation}rad`);
+    card.style.setProperty('--brightness', brightness);
+  });
+});
